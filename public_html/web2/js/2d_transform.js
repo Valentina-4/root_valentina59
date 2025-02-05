e7bdf484f29627129840928486fc6c63a8661049
Valentina <6316759@edu.vaniercollege.qc.ca>
2025-02-05 10:07:33 -0500

    cube

---
+++ public_html/web2/js/2d_transform.js
@@ -0,0 +1,8 @@
+console.log('2d_transform.js');
+
+let pinkmountain = document.querySelector("#pinkmountain");
+
+pinkmountain.addEventListener('click', function (event) {
+    //console.log(this.id);
+    this.classList.add('move-right');
+}) 
