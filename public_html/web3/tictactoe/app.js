e99ca21a24a9ab762c3022d600a2a15b68755d1a
Valentina-4 <6316759@edu.vaniercollege.qc.ca>
2025-08-27 15:54:07 -0400

    tictactoe game

---
+++ public_html/web3/tictactoe/app.js
@@ -0,0 +1,73 @@
+
+// . is to target class, # is to target id
+
+let cases = document.querySelectorAll(".cases")
+let replayBtn = document.querySelector("#replay")
+let paneauMessage = document.querySelector("#message")
+
+// variables de l'app
+let jouerX = true;
+let gagnant = '';
+const patrons = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+
+for (let boite of cases) {
+        boite.active = true;
+        boite.addEventListener("click", function(){
+            if (boite.active) {
+                if (jouerX) {
+                    boite.innerText = "X";
+                    jouerX = false;
+             }
+
+            else{
+                boite.innerText = "O"
+                jouerX = true;
+            }
+    
+        }
+        boite.active = false
+        valide();
+    });
+
+}
+
+
+const valide = function () {
+    for(let patron of patrons) {
+        let val1 = cases[patron[0]].innerText;
+        let val2 = cases[patron[1]].innerText;
+        let val3 = cases[patron[2]].innerText;
+
+        if (val1 &&
+            val1 === val2 &&
+            val1 === val3) { 
+            console.log(`Le gagnant est ${val1}`); 
+            for (let boite of cases) {
+                boite.active = false;
+            }
+        }
+    }
+}
+
+
+    replayBtn.addEventListener("click", function (){
+        for (let boite of cases) {
+            boite.active = true
+            boite.innerText = "";
+            jouerX = true
+        }
+    })
+    
+
+   
+
